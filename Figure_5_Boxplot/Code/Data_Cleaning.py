d546e6fdf9af5cc41b4aba45f68a194b6a9e502b
Yifan Wu <34715745+yifanwu0909@users.noreply.github.com>
2018-12-14 15:57:27 -0500

    Update Data_Cleaning.py

--- Figure_5_Boxplot/Code/Data_Cleaning.py
+++ Figure_5_Boxplot/Code/Data_Cleaning.py
@@ -4,6 +4,6 @@ df = df.sample(frac=0.05)
 df = df.dropna(subset=["PINCP"])
 df = df[df["PINCP"] != 0.0]
 df_income = df[["PINCP", "MAR"]]
-df_income.to_csv("income_marriage300.csv")
+df_income.to_csv("income_marriage300_boxplot_jitter.csv")
 
 
